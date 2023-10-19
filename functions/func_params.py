81d93d1e665cad6ace3b3ea4b6c050b7a7f2b1bf
Gaurav Shukla <gaurav.shukla.dev12@gmail.com>
2023-10-20 00:39:25 +0530

    added function code

---
+++ functions/func_params.py
@@ -0,0 +1,12 @@
+# function with normal arguments
+
+def add(val_a,val_b):
+    return val_a + val_b
+
+print(add(5,19))
+
+# function with defaul arguments
+def add_default(val_a,val_b = 10):
+    return val_a + val_b
+
+print(add_default(5))
